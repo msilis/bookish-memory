70657257e9c732f76cfccbb2a719961abc890757
msilis <mikssilis@gmail.com>
2026-03-20 22:26:28 +0000

    node modules cleanup

--- engine.d.ts
+++
@@ -1,3 +0,0 @@
-import type { SiteRecipe } from "./types.ts";
-export declare function captureScreenshot(url: string, recipe: SiteRecipe): Promise<string | null>;
-//# sourceMappingURL=engine.d.ts.map
